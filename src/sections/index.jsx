137d1169a39960ab130d8530cd5fd44bc86def65
Shivam <shivamv1332@gmail.com>
2023-08-29 03:36:28 +0530

    initial commit

---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Services from "./Services"
+import SpecialOffer from "./SpecialOffer"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+
+export {
+    CustomerReviews,
+    PopularProducts,
+    Footer,
+    Hero,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
